864b0b89e5154a3aeeed0e427d09cdef024e0eaf
Johan Ahlen <johan@cloudera.com>
2015-09-17 16:56:40 +0200

    [notebook] Switch from double click to alt/ctrl + click to follow ace links
    Double click is often used to select whole words in text (triple click for line and quadruple click for document), this doesn't work if the table and file links are trigged on double click.

--- desktop/core/src/desktop/static/desktop/js/ko.hue-bindings.js
+++ desktop/core/src/desktop/static/desktop/js/ko.hue-bindings.js
@@ -1786,7 +1786,7 @@ ko.bindingHandlers.aceEditor = {
         this.onClick = this.onClick.bind(this);
         Event.addListener(editor.renderer.scroller, "mousemove", this.onMouseMove);
         Event.addListener(editor.renderer.content, "mouseout", this.onMouseOut);
-        Event.addListener(editor.renderer.content, "dblclick", this.onClick);
+        Event.addListener(editor.renderer.content, "click", this.onClick);
       };
 
       Oop.inherits(HueLink, Tooltip);
@@ -1863,7 +1863,7 @@ ko.bindingHandlers.aceEditor = {
         };
 
         this.onClick = function (e) {
-          if (this.link) {
+          if (this.link && (e.altKey || e.metaKey)) {
             this.link.editor = this.editor;
             this._signal("open", this.link);
             this.clear()
